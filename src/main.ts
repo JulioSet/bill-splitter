35d546776f84515d3069058e8f69c6803d090e0f
Julio <105445347+JulioSet@users.noreply.github.com>
2026-07-07 12:44:36 +0700

    installing packages

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
+import './style.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+// PrimeVue
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+// Prime Icons
+import 'primeicons/primeicons.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// PrimeVue
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
 
 app.mount('#app')
