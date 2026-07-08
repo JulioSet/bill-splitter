07ee3ae0e823579c548421ee13d6c569f84820db
Julio <105445347+JulioSet@users.noreply.github.com>
2026-07-08 09:16:17 +0700

    improve memory management

--- src/shared/lib/ocr.ts
+++ src/shared/lib/ocr.ts
@@ -2,26 +2,31 @@ import { createWorker, PSM } from 'tesseract.js'
 import { preprocessImage } from './imagePreprocessed'
 
 export async function recognizeReceipt(image: File, onProgress?: (progress: number) => void) {
-  const processed = await preprocessImage(image)
+  // const processed = await preprocessImage(image)
 
-  const worker = await createWorker('eng', undefined, {
-    logger(message) {
-      if (message.status === 'recognizing text' && message.progress) {
-        onProgress?.(Math.round(message.progress * 100))
-      }
-    },
-  })
+  let worker
+  if (typeof Worker !== 'undefined') {
+    worker = await createWorker('ind', undefined, {
+      logger(message) {
+        if (message.status === 'recognizing text' && message.progress) {
+          onProgress?.(Math.round(message.progress * 100))
+        }
+      },
+    })
 
-  await worker.setParameters({
-    tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
-    preserve_interword_spaces: '1',
-    textord_space_size_is_variable: '1',
-    tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,:-',
-  })
+    await worker.setParameters({
+      tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
+      preserve_interword_spaces: '1',
+      textord_space_size_is_variable: '1',
+      tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,:-',
+    })
+  }
 
-  const result = await worker.recognize(processed)
+  if (worker) {
+    const result = await worker.recognize(image)
 
-  await worker.terminate()
+    // await worker.terminate()
 
-  return result.data.text
+    return result.data.text
+  }
 }
